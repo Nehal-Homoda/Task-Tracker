63598c0920cfa2e64e88fbc9bdfc3b4ca2d3b759
nehal <nehalhomoda00@gmail.com>
2024-03-09 22:35:58 +0200

    components

---
+++ src/components/Header.vue
@@ -0,0 +1,29 @@
+<template>
+  <header>
+    <h1>{{ title }}</h1>
+    <Button @toggle-add-task="$emit('toggle-add-task')" :text="showAddTask ?'close':'Add Task'" :color="showAddTask ?'red':'green'" />
+    <Button text="Update Task" color="blue"/>
+    <Button text="Delete Task" color="red"/>
+  </header>
+</template>
+
+<script>
+import Button from "./Button.vue";
+export default {
+  name: "Header",
+  props: {
+    title: String,
+    showAddTask:Boolean
+  },
+  components: { Button },
+};
+</script>
+
+<style scoped>
+header {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  margin-bottom: 20px;
+}
+</style>
---
+++ src/components/Tasks.vue
@@ -0,0 +1,19 @@
+<template>
+  <div :key="task.id" v-for="task in tasks">
+    <task @toggle-reminder="$emit('toggle-reminder',task.id)" @delete-task="$emit('delete-task',task.id)" :task="task" />
+    
+    </div>  
+</template>
+<script>
+import task from './task.vue'
+export default {
+    name:"Tasks",
+    props:{
+        tasks:Array,
+    },
+    components:{
+        task
+    },
+    emits:['delete-task','toggle-reminder']
+}
+</script>
